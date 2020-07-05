54f80e61c5962be45a4e31095a43261e5f6a5eb6
alif33 <alifhasan332@gmail.com>
2020-07-05 03:15:11 -0700

    custom toolbar finished

--- toolbar/toolbar.js
+++ toolbar/toolbar.js
@@ -1,3 +1,7 @@
 var url = chrome.extension.getURL('toolbar.html');
+var height = '35px';
 var iframe = '<iframe src="'+url+'" id="myOwnCustomToolbar56778" height="35px" width=""></iframe>';
-$('body').append(iframe);
+$('html').append(iframe);
+$('body').css({
+    '-webkit-transform' : 'translateY('+height+')'
+});
